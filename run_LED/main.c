4aa9c16f3381060a4cdd18b5809baeaab6878157
TheLudwigWassman <lwn15002@student.mdh.se>
2022-11-14 16:17:26 +0100

    Sofia

--- run_LED/main.c
+++ run_LED/main.c
@@ -3,25 +3,31 @@
 #include <wiringPi.h>
 
 /* PWM Raspberry Pi 4 model B */
-int LED1 = 26;
-int LED2 = 23;
+int enA = 26; //BCM 12 || Physical 32
+int in1 = 4; //BCM 23 || Physical 16
+int in2 = 5; //BCM 24 || Physical 18
 
 int main() {
 
   printf ("Start:\n");
     wiringPiSetup();	/* initialize wiringPi setup */
 
-    pinMode(LED1,OUTPUT);	/* set GPIO as output */
-    pinMode(LED2,OUTPUT);	/* set GPIO as output */
+    pinMode(enA,OUTPUT);	/*set GPIO as output */
+    pinMode(in1,OUTPUT);
+    pinMode(in2,OUTPUT);
     
     while (1)
     {
-      digitalWrite(LED1,HIGH);		/* write high on GPIO */ 
-      digitalWrite(LED2,LOW);		/* write high on GPIO */ 
-      delay(1000);
 
-      digitalWrite(LED1,LOW);		/* write high on GPIO */ 
-      digitalWrite(LED2,HIGH);		/* write high on GPIO */ 
-      delay(3000);
+      //Forward in1 high and in2 low 
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      for (size_t i = 0; i < 1023; i++)
+      {
+        analogWrite(enA, i);
+        delay(5);
+      }
+      
+
     }
 }
